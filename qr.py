b80a9756384523c338a23877946b048b7130f492
uvk1603 <112807109+uvk1603@users.noreply.github.com>
2023-03-14 07:59:23 +0530

    Create qr.py
    Install Pillow and Qrcode modules using pip

---
+++ qr.py
@@ -0,0 +1,35 @@
+import qrcode
+from PIL import Image
+
+# taking image which user wants
+Logo_link = 'python.png'
+logo = Image.open(Logo_link)
+
+# taking base width
+base_width = 150
+# adjust image size
+size = (base_width/float(logo.size[0]))
+hsize = int((float(logo.size[1])*float(size)))
+logo = logo.resize((base_width, hsize), Image.ANTIALIAS)
+QRcode = qrcode.QRCode(
+	error_correction=qrcode.constants.ERROR_CORRECT_H
+)
+# taking url or text
+url = 'http://www.vnrvjiet.ac.in/'
+QRcode.add_data(url)
+QRcode.make()
+# taking color name from user
+QRcolor = 'Purple'
+
+# adding color to QR code
+QRimg = QRcode.make_image(
+    fill_color=QRcolor, back_color="white").convert('RGB')
+
+# set size of QR code
+pos = ((QRimg.size[0] - logo.size[0]) // 2,
+    (QRimg.size[1] - logo.size[1]) // 2)
+QRimg.paste(logo, pos)
+
+# save the QR code generated
+QRimg.save('logo.png')
+print('QR code generated!')
